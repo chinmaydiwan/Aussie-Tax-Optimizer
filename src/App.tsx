0e25d649b76268d9e86ee1cd32bd585ea72adba0
chinmaydiwan <chinmaydiwan@yahoo.co.in>
2025-05-20 15:48:12 +1000

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { TaxDataProvider } from "./contexts/TaxDataContext";
 import Index from "./pages/Index";
 import PersonalDetails from "./pages/PersonalDetails";
@@ -20,7 +20,7 @@ const App = () => (
       <TaxDataProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/personal" element={<PersonalDetails />} />
@@ -30,7 +30,7 @@ const App = () => (
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TaxDataProvider>
     </TooltipProvider>
   </QueryClientProvider>
